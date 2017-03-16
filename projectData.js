a3b5d2607e8c202519235266ab5edae88235459c
Kurt Maurer <kurtrm@gmail.com>
2017-03-16 10:41:53 -0700

    Populated an array of my first three projects with data.

---
+++ projectData.js
@@ -0,0 +1,28 @@
+'use-strict';
+
+var projectData = [
+  {
+    title: 'Salmon Cookies',
+    link: 'https://kurtrm.github.io/Salmon-Cookies/',
+    description: 'This is a silly project that I worked on during my time at Code Fellows. I learned to use raw DOM manipulation in order to display data in a table useful to an imaginary business.',
+    contributors: '',
+    image: 'http://placekitten.com/g/200/300',
+  },
+
+  {
+    title: 'time-wizards',
+    link: 'https://kurtrm.github.io/time-wizards/',
+    description: 'This was a final project for our first course at Code Fellows. We worked on a multi-page web RPG that implemented the skills and knowledge we learned throught our time in the 201 course.',
+    contributors: ['Danny', 'Michael', 'Brianna', 'Glen'],
+    image: 'http://placekitten.com/g/200/300',
+  },
+
+  {
+    title: 'Bus Mall',
+    link: 'https://kurtrm.github.io/busmall/',
+    description: 'This project implemented new libraries and tools we learned, primarily localStorage and charts.js. I also used some basic CSS transform properties to make it look nice and appealing to the user.',
+    contributors: '',
+    image: 'http://placekitten.com/g/200/300',
+  },
+
+];
