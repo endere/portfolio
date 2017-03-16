69aa4be8ffe110475a3980c0cd20e32d6e6844f2
Kurt Maurer <kurtrm@gmail.com>
2017-03-16 12:19:33 -0700

    Got the projects to append to the page. Need to strip the template, make each project a link, and fine tune the details for each project section. Also, add the image as a background image.

--- projects.js
+++ projects.js
@@ -2,6 +2,8 @@
 
 //This file is for the constructors of the individual projects on my portfolio page.
 
+allProjects = [];
+
 function Project (props) {
   this.title = props.title;
   this.link = props.link;
@@ -9,3 +11,34 @@ function Project (props) {
   this.contributors = props.contributors;
   this.image = props.image;
 }
+
+Project.prototype.toHtml = function() {
+  //Create a template in the html for this stuff!
+  //Needs: .template data-''
+  var $newProject = $('article.template').clone().removeClass('template');
+
+  // $newProject.attr('', this.);
+
+  $newProject.find('.title').text(this.title);
+  $newProject.find('.link').attr('href', this.link);
+  $newProject.find('.description').text(this.description);
+  // $newProject.find('.contributors').html(this.contributors);
+  // $newProject.find('').attr('datetime', this.);
+  $('#projects').css('background-image' , this.image);
+  // $newProject.find('').text('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
+  return $newProject;
+};
+
+// projectData.sort(function(a,b) {
+//   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+// });
+
+projectData.forEach(function(ele) {
+  allProjects.push(new Project(ele));
+  console.table(allProjects);
+});
+
+allProjects.forEach(function(a){
+  //projects id for where to put them.
+  $('#projects').append(a.toHtml());
+});
