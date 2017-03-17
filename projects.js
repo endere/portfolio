a5bf3bd3d729c06f082b479249e3bfff2d93904c
Kurt Maurer <kurtrm@gmail.com>
2017-03-17 12:14:32 -0700

    Refactored CSS.Still working on functionality.

--- projects.js
+++ projects.js
@@ -13,23 +13,12 @@ function Project (props) {
 }
 
 Project.prototype.toHtml = function() {
-  //Create a template in the html for this stuff!
-  //Needs: .template data-''
-  // var $newProject = $('article.template').clone().removeClass('template');
-
-  // $newProject.attr('', this.);
   $('.template').hide();
   var source = $('#project-template').html();
   var templateRender = Handlebars.compile(source);
 
-  // $newProject.find('.title').text(this.title);
-  // $newProject.find('.link').attr('href', this.link);
-  // $newProject.find('.description').text(this.description);
-  // // $newProject.find('.contributors').html(this.contributors);
-  // // $newProject.find('').attr('datetime', this.);
   // $('#projects').css('background-image' , this.image);
   // // $newProject.find('').text('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
-  // return $newProject;
 
   return templateRender(this);
 
@@ -50,4 +39,10 @@ projectData.forEach(function(ele) {
 allProjects.forEach(function(a){
   //projects id for where to put them.
   $('#projects').append(a.toHtml());
+});
+
+$('.hamburger').on('hover', function() {
+  $('ul').css('display', 'block');
+  console.log($('ul').css());
+  $('.nav-list').css('display', 'block');
 });
